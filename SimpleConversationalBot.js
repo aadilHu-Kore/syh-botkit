4626bfe3bae672b225b87822b8976678c68fe6f5
SubashMourougayane <msubashjay@gmail.com>
2022-11-04 16:28:23 +0530

    removed unwanted logs

--- SimpleConversationalBot.js
+++ SimpleConversationalBot.js
@@ -1,41 +1,21 @@
-//var botId = ["st-980bad94-cb6d-57c2-8a78-a767d037fb02","st-c8c35fee-9e41-5f9e-a3a1-952dd11cc9d8"];
-//const botName = ["SYH_DEV","SYH_Spanish"];
 const config = require('./config.json');
 const botId = Object.keys(config.credentials);
 const botName = botId.map(id => config.credentials[id].botName);
 var sdk = require("./lib/sdk");
 
-/*
- * This is the most basic example of BotKit.
- *
- * It showcases how the BotKit can intercept the message being sent to the bot or the user.
- *
- * We can either update the message, or chose to call one of 'sendBotMessage' or 'sendUserMessage'
- */
+
 module.exports = {
     botId: botId,
     botName: botName,
 
     on_user_message: function (requestId, data, callback) {
-
-        if (data.message === "Hi" || data.message === "hello") {
-            data.message = "Hello Aadil from Botkit";
-
-            //Sends back 'Hello' to user.
-            //return sdk.sendBotMessage(data, callback);
-            return sdk.sendUserMessage(data, callback);
-            // } else if (!data.agent_transfer) {
-            //Forward the message to bot
-            //     return sdk.sendBotMessage(data, callback);
-            // } 
-        }
     
 
         if (data.context.session.BotUserSession.setLanguageOverrideFlag === true || data.channel.botEvent === 'ON_CONNECT_EVENT') {
             data.metaInfo = { setBotLanguage: 'en' };
         }
-        // if(data.channel.botEvent === 'ON_CONNECT_EVENT' && data.context.session.BotUserSession.setLanguageOverrideFlag === true)
-        //SYH_Spanish -> 12055258902, //VCC_QA_Spanish -> 12513060520, DID for Spanish = 8858 SYH_English -> +12057363676
+
+
         //------------------SAT VOICE START------------------------------------------------------------------ 
         if (data.context.session.BotUserSession.channels[0].type == 'smartassist' && data.context.session.UserSession.DialedNumber == '+12512766105') {
 
@@ -101,11 +81,6 @@ module.exports = {
         console.log("caller number on BOT EVENT:", data.context.session.UserSession.Caller)
         console.log("dailed number on BOT EVENT:", data.context.session.UserSession.DialedNumber)
         
-        if (data.message === 'hello') {
-            data.message = 'The Bot says hello!';
-        }
-        //Sends back the message to user
-
         return sdk.sendUserMessage(data, callback);
     },
     on_agent_transfer: function (requestId, data, callback) {
