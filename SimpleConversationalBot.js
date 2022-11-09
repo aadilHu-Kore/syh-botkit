8ef4f18f867e79432cb3800761106de1d0175b0b
aadilHu-Kore <aadil.hussain@kore.com>
2022-11-09 17:49:21 +0530

    added a place holder for DID 4 digit check and removed the SATWelcomeDialog trigger for voice

--- SimpleConversationalBot.js
+++ SimpleConversationalBot.js
@@ -3,22 +3,28 @@ const botId = Object.keys(config.credentials);
 const botName = botId.map(id => config.credentials[id].botName);
 var sdk = require("./lib/sdk");
 
+
 module.exports = {
     botId: botId,
     botName: botName,
 
     on_user_message: function (requestId, data, callback) {
+    
 
         if (data.context.session.BotUserSession.setLanguageOverrideFlag === true || data.channel.botEvent === 'ON_CONNECT_EVENT') {
             data.metaInfo = { setBotLanguage: 'en' };
         }
+
+
         //------------------SAT VOICE START------------------------------------------------------------------ 
-        if (data.context.session.BotUserSession.channels[0].type == 'smartassist' && data.context.session.UserSession.DialedNumber == '+12512766105') {
+        if (data.context.session.BotUserSession.channels[0].type == 'smartassist' && (data.context.session.UserSession.DialedNumber == '+12512766105' || data.context.session.UserSession.DialedNumber == '8858')) {
+
             data.metaInfo = { setBotLanguage: 'es' };
             data.context.session.BotUserSession.setLanguageOverrideFlag === false;
         }
 
-        else if (data.context.session.BotUserSession.channels[0].type == 'smartassist' && data.context.session.UserSession.DialedNumber == '+12057363676') {
+        else if (data.context.session.BotUserSession.channels[0].type == 'smartassist' && (data.context.session.UserSession.DialedNumber == '+12512766105' && data.context.session.UserSession.DialedNumber == '8808')) {
+            
             data.metaInfo = { setBotLanguage: 'en' };
             data.context.session.BotUserSession.setLanguageOverrideFlag === false;
         }
@@ -26,9 +32,13 @@ module.exports = {
 
         //------------------WEB START------------------------------------------------------------------ 
         else if (data.context.session.BotUserSession.channels[0].type == 'rtm' && data.context.session.BotUserSession.setLanguageOverrideFlag === true) {
+
+
             if (data.message !== undefined) {
-                console.log("user input::", data.message)
+    
                 if (data.message.toLowerCase().includes("english") || data.message.toLowerCase().includes("spanish")) {
+        
+        
                     var lang = {
                         "english": "en",
                         "spanish": "es",
@@ -42,7 +52,7 @@ module.exports = {
                             'isRefresh': true
                         }
                     };
-                    console.log("test::", data.metaInfo)
+        
                 }
             }
         }
@@ -51,8 +61,13 @@ module.exports = {
         //------------------Agent Mode Exit END------------------------------------------------------------------
         if (data && data.agent_transfer && (data.message === "####" || data.message === "abandonar" || data.message === "quit" || data.message === "stop chat")) {
             data.message = "Ok, exiting the agent mode.";
-            sdk.sendUserMessage(data, callback);
             sdk.clearAgentSession(data);
+            return sdk.sendUserMessage(data);
+
+        }
+
+        if(data.agent_transfer){
+            return sdk.sendBotMessage(data, callback)
         }
         //------------------Agent Mode Exit END------------------------------------------------------------------
         return sdk.sendBotMessage(data, callback);
@@ -63,26 +78,16 @@ module.exports = {
         console.log("Language override Flag value on BOT EVENT::", data.context.session.BotUserSession.setLanguageOverrideFlag)
         console.log("caller number on BOT EVENT:", data.context.session.UserSession.Caller)
         console.log("dailed number on BOT EVENT:", data.context.session.UserSession.DialedNumber)
-        // console.log('Bot Request ID==>', requestId);
-        // console.log('ON_BOT_MESSAGE => ', data.message);
-        console.log("intent triggered::", data.context.intent);
-        if (data.message === 'hello') {
-            data.message = 'The Bot says hello!';
-        }
-        //Sends back the message to user
-
+        
         return sdk.sendUserMessage(data, callback);
     },
     on_agent_transfer: function (requestId, data, callback) {
-        console.log("on_event -->  Event : ", data.event);
         return callback(null, data);
     },
     on_event: function (requestId, data, callback) {
-        console.log("on_event -->  Event : ", data.event);
         return callback(null, data);
     },
     on_alert: function (requestId, data, callback) {
-        console.log("on_alert -->  : ", data, data.message);
         return sdk.sendAlertMessage(data, callback);
     }
 
